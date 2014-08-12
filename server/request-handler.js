afc975427c35315715dc19b8cde1d26fe9a1b3e1
codengjon <1jonathan.ng@gmail.com>
2014-08-11 19:43:43 -0700

    Passes specs

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,21 +4,45 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var exports = module.exports = {};
 
-var handleRequest = function(request, response) {
+var results = [];
+
+exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
+  // console.log(request);
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+  // var statusCode = 200;
+  var statusCode;
+  var data = "";
+
+  if(request.url === '/classes/messages' || request.url === "/classes/room1"){
+    if(request.method === 'GET') {
+      statusCode = 200;
+    } else if(request.method === 'POST') {
+      statusCode = 201;
+      request.on("data", function(chunk){
+        data += chunk;
+      });
+      request.on("end", function(){
+        results.push(JSON.parse(data));
+        console.log(results);
+      });
+    }
+  } else {
+    statusCode = 404;
+  }
+
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
+  var headers = exports.defaultCorsHeaders;
 
   headers['Content-Type'] = "text/plain";
 
@@ -29,7 +53,10 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+
+  response.end(JSON.stringify({results: results}));
+  // response.end("Hella world!");
+
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -37,7 +64,7 @@ var handleRequest = function(request, response) {
  * are on different domains. (Your chat client is running from a url
  * like file://your/chat/client/index.html, which is considered a
  * different domain.) */
-var defaultCorsHeaders = {
+exports.defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
